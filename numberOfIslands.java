f51be5495f10539bb8ae254014d3db1d3991509b
Akshat Danve <akshatdanve47@gmail.com>
2026-06-16 12:16:44 +0530

    Leetcode problem 200

--- numberOfIslands.java
+++ numberOfIslands.java
@@ -1,32 +1,47 @@
-class Solution {
-    public int numIslands(char[][] grid) {
-        if (grid==null || grid.length==0){
-            return 0;
-        }
+class numberOfIslands {
 
-        int numIslands=0;
-        for (int i=0; i<grid.length; i++){
-            for (int j=0; j<grid[0].length; j++){
-                if (grid[i][j]=='1'){
-                    numIslands++;
-                    dfs(grid,i,j);
+    public int numIslands(char[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        boolean[][] vis = new boolean[m][n];
+        int count = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+                // Found an unvisited piece of land
+                if (grid[i][j] == '1' && !vis[i][j]) {
+                    dfs(grid, vis, i, j);
+                    count++;
                 }
             }
         }
 
-        return numIslands;
- 
+        return count;
     }
 
-    private void dfs(char[][] grid, int i, int j){
-        if (i<0 || i>=grid.length || j<0 || j>=grid[0].length || grid[i][j]!='1'){
+    private void dfs(char[][] grid, boolean[][] vis, int row, int col) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        // Out of bounds
+        if (row < 0 || row >= m || col < 0 || col >= n) {
             return;
         }
 
-        grid[i][j]='0';
-        dfs(grid,i+1,j);
-        dfs(grid,i-1,j);
-        dfs(grid,i,j+1);
-        dfs(grid,i,j-1);
+        // Water or already visited
+        if (grid[row][col] == '0' || vis[row][col]) {
+            return;
+        }
+
+        // Mark current cell as visited
+        vis[row][col] = true;
+
+        // Explore all 4 directions
+        dfs(grid, vis, row - 1, col); // Up
+        dfs(grid, vis, row + 1, col); // Down
+        dfs(grid, vis, row, col - 1); // Left
+        dfs(grid, vis, row, col + 1); // Right
     }
 }
