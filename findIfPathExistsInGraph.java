fda128a6d292eaeaba50a391b5702f03b2c5c577
Akshat Danve <akshatdanve47@gmail.com>
2026-06-18 15:54:00 +0530

    Leetcode problem 1971

--- findIfPathExistsInGraph.java
+++ findIfPathExistsInGraph.java
@@ -1,3 +1,4 @@
+//dfs approach
 class findIfPathExistsInGraph {
     public boolean validPath(int n, int[][] edges, int source, int destination) {
         ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>();
@@ -32,3 +33,46 @@ class findIfPathExistsInGraph {
         return false;
     }
 }
+
+//bfs approach
+/*
+class Solution {
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for(int i=0; i<n; i++){
+            adj.add(new ArrayList<>());
+        }
+        
+        for(int[] edge : edges){
+            int u = edge[0];
+            int v = edge[1];
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+            
+        }
+
+        
+        boolean[] vis = new boolean[n];
+        Queue<Integer> q = new LinkedList<>();
+        ArrayList<Integer> res = new ArrayList<>();
+        vis[source]=true;
+        q.add(source);
+        while(!q.isEmpty()){
+            int curr = q.poll();
+            res.add(curr);
+            for(int x : adj.get(curr)){
+                if(!vis[x]){
+                    vis[x]=true;
+                    q.add(x);
+                }   
+            }
+        }
+        for(int i=0; i<res.size(); i++){
+            if(res.get(i)==destination){
+                return true;
+            }
+        }
+        return false;
+    }
+}*/
+
