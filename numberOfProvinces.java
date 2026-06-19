eeff5fa3f93cc1c01c7252798c6e93d56e77a723
Akshat Danve <akshatdanve47@gmail.com>
2026-06-19 14:40:33 +0530

    Leetcode problem 547

--- numberOfProvinces.java
+++ numberOfProvinces.java
@@ -1,14 +1,45 @@
+//dfs approach
 class numberOfProvinces {
     public int findCircleNum(int[][] isConnected) {
-        int provinces = 0;
+        int provinces=0;
+        int n = isConnected.length;
+        boolean[] visited = new boolean[n];
+        for(int i=0; i<n; i++){
+            if(!visited[i]){
+                provinces++;
+                dfs(i, isConnected, visited);
+            }
+        }
+        return provinces;
+    }
+    void dfs(int node, int[][] isConnected, boolean[] visited){
+        visited[node]=true;
+        for(int i=0; i<isConnected.length; i++){
+            if(isConnected[node][i]==1 && !visited[i]){
+                dfs(i, isConnected, visited);
+            }
+        }
+    }
+}
+
+
+
+
+
+
+//bfs approach
+/*
+class numberOfProvinces {
+    public int findCircleNum(int[][] isConnected) {
+        int provinces=0;
         int n = isConnected.length;
         boolean[] vis = new boolean[n];
         for(int i=0; i<n; i++){
-            if(!vis[i]){
+            while(!vis[i]){
                 provinces++;
                 Queue<Integer> q = new LinkedList<>();
                 q.add(i);
-                vis[i]=true;    
+                vis[i]=true;
                 while(!q.isEmpty()){
                     int curr = q.poll();
                     for(int j=0; j<n; j++){
@@ -18,8 +49,10 @@ class numberOfProvinces {
                         }
                     }
                 }
+                
             }
         }
         return provinces;
     }
 }
+*/
